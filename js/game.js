aa089e7aa10d04b69a57b9a9acb40cc4fd54e7b7
Sveva Grippa <sveva.marisa@yahoo.it>
2024-10-11 14:39:01 +0200

    final version commit

--- js/game.js
+++ js/game.js
@@ -1,7 +1,7 @@
 let config = {
     type: Phaser.AUTO,
-    width: 480,
-    height: 320,
+    width: 960,
+    height: 640,
     physics: {
         default: 'arcade'
     },
@@ -21,19 +21,19 @@ let config = {
 let game = new Phaser.Game(config);
 
 let titleScreenImage;
-let froggy, deadFrog;
-let mumFrog;
+let dragon, deadFrog;
+let cave;
 let downArrow, upArrow, leftArrow, rightArrow;
 let quack, smash, heart;
-let car=[];
-let twoCar=[];
+let birds=[];
+let otherBirds=[];
 let money=[];
 let score, countDown, scoreText, countDownText;
 let playButtonImage;
 
 let countDownTimer, STARTCOUNTDOWN;
 
-let carNBinArow, rowNB;
+let birdNBinArow, rowNB;
 
 let bestScores=[];
 
@@ -41,7 +41,7 @@ let playing;
 
 //
 function init() {
-    carNBinArow=5;
+    birdNBinArow=5;
     rowNB= 2;
     score=0;
     STARTCOUNTDOWN=60;
@@ -50,11 +50,11 @@ function init() {
 
 function preload() {
     this.load.image('background', './assets/images/new_background.jpg');
-    this.load.image('titlescreen', './assets/images/dragonTitleScreen.png');
+    this.load.image('titlescreen', './assets/images/new_titlescreen2.jpg');
 
-    this.load.image('frog', './assets/images/Dragon.png');
+    this.load.image('dragon', './assets/images/Dragon.png');
 
-    this.load.image('mum', './assets/images/new_cave.png');
+    this.load.image('cave', './assets/images/new_cave.png');
 
     this.load.image('heart', './assets/images/heart.png');
 
@@ -70,7 +70,7 @@ function preload() {
 
     this.load.image('coin', './assets/images/coin.png');
 
-    this.load.image('playButton', './assets/images/playButton.webp');
+    this.load.image('playButton', './assets/images/playButtonCustom.png');
 
     this.load.audio('quack', './assets/audio/coaac.wav');
 
@@ -78,53 +78,54 @@ function preload() {
 }
 
 function create() {
-    let backImage= this.add.image(0,0, 'background');
+    let backImage= this.add.image(0,1, 'background');
     backImage.setOrigin(0,0);
     // backImage.setScale(5);
 
-    froggy= this.add.image(8+Phaser.Math.Between(0, 29)*16,312, 'frog');
+    dragon= this.add.image(12+Phaser.Math.Between(0, 60)*16, 610, 'dragon');
     // froggy.setOrigin(0,0);
-    // froggy.setScale(2);
+    //froggy.setScale(2);
 
-    mumFrog= this.add.image(8+Phaser.Math.Between(0, 22)*16,8, 'mum');
+    cave= this.add.image(8+Phaser.Math.Between(0, 55)*16,28, 'cave');
 
-    deadFrog= this.add.image(froggy.x,froggy.y, 'deadfrog');
+    deadFrog= this.add.image(dragon.x,dragon.y, 'deadfrog');
     deadFrog.setVisible(false);
 
-    let space =config.width/ carNBinArow;
+    let space =config.width/ birdNBinArow;
 
-    money[0]= this.add.image(Phaser.Math.Between(12, 450),235, 'coin');
+    money[0]= this.add.image(Phaser.Math.Between(12, 950),60, 'coin');
 
-    money[1]= this.add.image(Phaser.Math.Between(12, 450),175, 'coin');
+    money[1]= this.add.image(Phaser.Math.Between(12, 950),195, 'coin');
 
-    money[2]= this.add.image(Phaser.Math.Between(12, 450),110, 'coin');
+    money[2]= this.add.image(Phaser.Math.Between(12, 950),320, 'coin');
 
-    money[3]= this.add.image(Phaser.Math.Between(12, 380),30, 'coin');
+    money[3]= this.add.image(Phaser.Math.Between(12, 950),445, 'coin');
 
     for (let j = 0; j < rowNB; j++) {
 
-        for (let i = 0; i < carNBinArow; i++) {
-            car[i+j*carNBinArow]= this.physics.add.image(i*space + Phaser.Math.Between(-10,10),264-128*j, 'car' + Phaser.Math.Between(1,3));
-            car[i+j*carNBinArow].setVelocity(70,0);
+        for (let i = 0; i < birdNBinArow; i++) {
+            birds[i+j*birdNBinArow]= this.physics.add.image(i*space + Phaser.Math.Between(-10,10),500-250*j, 'car' + Phaser.Math.Between(1,3));
+            birds[i+j*birdNBinArow].setVelocity(70,0);
         }
     }
 
     for (let k = 0; k < rowNB; k++) {
 
-        for (let i = 0; i < carNBinArow; i++) {
-            twoCar[i+k*carNBinArow]= this.physics.add.image(i*space + Phaser.Math.Between(-10,10),205-128*k, 'car' + Phaser.Math.Between(4,6));
-            twoCar[i+k*carNBinArow].setVelocity(-70,0);
+        for (let i = 0; i < birdNBinArow; i++) {
+            otherBirds[i+k*birdNBinArow]= this.physics.add.image(i*space + Phaser.Math.Between(-10,10),380-250*k, 'car' + Phaser.Math.Between(4,6));
+            otherBirds[i+k*birdNBinArow].setVelocity(-70,0);
         }
     }
 
-    heart= this.add.image(240,180, 'heart');
+    heart= this.add.image(430,180, 'heart');
     heart.setScale(0);
 
     heartTween = this.tweens.add({
         targets: heart,
-        y: 100,
-        scaleY: 2,
-        scaleX: 2,
+        y: 400,
+        x: 500,
+        scaleY: 4,
+        scaleX: 4,
         duration: 2000,
         ease: 'linear',
         yoyo : true,
@@ -139,9 +140,9 @@ function create() {
     leftArrow= this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
     rightArrow= this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
 
-    scoreText= this.add.text(410, 0, 'Score: '+ score, { fontFamily: 'Arial', fontSize: 15, color: ' #FFFFFF'});
+    scoreText= this.add.text(880, 0, 'Score: '+ score, { fontFamily: 'Arial', fontSize: 20, color: ' #FFFFFF'});
 
-    countDownText= this.add.text(420, 30, countDown + 's', { fontFamily: 'Arial', fontSize: 15, color: ' #FFFFFF'});
+    countDownText= this.add.text(920, 30, countDown + 's', { fontFamily: 'Arial', fontSize: 20, color: ' #FFFFFF'});
 
     countDownTimer= this.time.addEvent({
         delay: 1000, // ms
@@ -152,60 +153,66 @@ function create() {
         paused: true
     })
 
-    titleScreenImage= this.add.image(0,0, 'titlescreen');
+    titleScreenImage= this.add.image(0.6,1, 'titlescreen');
     titleScreenImage.setOrigin(0,0);
-    titleScreenImage.setScale(0.68);
+    titleScreenImage.setScale(0.75);
 
-    bestScoreText1= this.add.text(190, 155, 'Premier:  '+ bestScores[0], { fontFamily: 'Arial', fontSize: 15, color: ' #ff0000'});
-    bestScoreText2= this.add.text(190, 195, 'Deuxieme:  '+ bestScores[1], { fontFamily: 'Arial', fontSize: 15, color: ' #ff0000'});
-    bestScoreText3= this.add.text(190, 235, 'Troisieme:  '+ bestScores[2], { fontFamily: 'Arial', fontSize: 15, color: ' #ff0000'});
+    bestScoreText1= this.add.text(400, 325, 'First:  '+ bestScores[0], { fontFamily: 'Arial', fontSize: 30, color: ' #ff0000'});
+    bestScoreText2= this.add.text(400, 385, 'Second:  '+ bestScores[1], { fontFamily: 'Arial', fontSize: 30, color: ' #ff0000'});
+    bestScoreText3= this.add.text(400, 450, 'Third:  '+ bestScores[2], { fontFamily: 'Arial', fontSize: 30, color: ' #ff0000'});
 
-    playButtonImage= this.add.image(240,290, 'playButton').setInteractive();
-    playButtonImage.setScale(0.05);
-    playButtonImage.on('pointerdown', startGame)
+    playButtonImage= this.add.image(480,550, 'playButton').setInteractive();
+    playButtonImage.setScale(0.1);
+    playButtonImage.on('pointerdown', startGame);
+
+
+    // this.physics.add.collider(froggy, car, collisionCarPlayer, null, this);
+
+    // this.physics.add.collider(froggy, twoCar, collisionTwoCarPlayer, null, this);
+
 }
 
 function update() {
-
+    
     //Verifier si la voiture sort à droite de l'écran
 
-    for (let i = 0; i < carNBinArow*rowNB; i++) {
-        if(car[i].x>480) car[i].x = 0;
+    for (let i = 0; i < birdNBinArow*rowNB; i++) {
+        if(birds[i].x>1000) birds[i].x = 0;
     }
 
-    for (let h = 0; h < carNBinArow*rowNB; h++) {
-        if(twoCar[h].x<0) twoCar[h].x = 480;
+    for (let h = 0; h < birdNBinArow*rowNB; h++) {
+        if(otherBirds[h].x<0) otherBirds[h].x = 1000;
     }
 
     //JustDown pour verifier
-    if(Phaser.Input.Keyboard.JustDown(upArrow)&& froggy.y>8){
-        froggy.y-=16;
-        froggy.angle= 0;
+    if(Phaser.Input.Keyboard.JustDown(upArrow)&& dragon.y>8){
+        dragon.y-=26;
+        dragon.angle= 0;
         quack.play();
     };
 
-    if(Phaser.Input.Keyboard.JustDown(downArrow)&& froggy.y<312){
-        froggy.y+=16;
-        froggy.angle=180;
+    if(Phaser.Input.Keyboard.JustDown(downArrow)&& dragon.y<630){
+        dragon.y+=26;
+        dragon.angle=180;
         quack.play();
     };
 
-    if(Phaser.Input.Keyboard.JustDown(leftArrow)&& froggy.x>8){
-        froggy.x-=16;
-        froggy.angle=270;
+    if(Phaser.Input.Keyboard.JustDown(leftArrow)&& dragon.x>8){
+        dragon.x-=26;
+        dragon.angle=270;
         quack.play();
     };
 
-    if(Phaser.Input.Keyboard.JustDown(rightArrow)&& froggy.x<472){
-        froggy.x+=16;
-        froggy.angle=90;
+    if(Phaser.Input.Keyboard.JustDown(rightArrow)&& dragon.x<950){
+        dragon.x+=26;
+        dragon.angle=90;
         quack.play();
     };
 
 
-    if(Phaser.Geom.Intersects.RectangleToRectangle(froggy.getBounds(), mumFrog.getBounds())) {
+    if(Phaser.Geom.Intersects.RectangleToRectangle(dragon.getBounds(), cave.getBounds())) {
         heartTween.play();
-        froggy.x= -1000;
+        dragon.x= -1000;
         // score+=1;
         // scoreText.text= "Score: "+ score;
 
@@ -218,10 +225,11 @@ function update() {
         })
 
         resetMoneyPosition();
+
     };
 
     for (let m = 0; m < 4; m++) {
-        if(Phaser.Geom.Intersects.RectangleToRectangle(froggy.getBounds(), money[m].getBounds())) {
+        if(Phaser.Geom.Intersects.RectangleToRectangle(dragon.getBounds(), money[m].getBounds())) {
             // smash.play();
     
             money[m].x=-1000;
@@ -230,24 +238,24 @@ function update() {
         };
     }
 
-    for (let i = 0; i < carNBinArow*rowNB; i++) {
+    for (let i = 0; i < birdNBinArow*rowNB; i++) {
         
-        if(Phaser.Geom.Intersects.RectangleToRectangle(froggy.getBounds(), car[i].getBounds())) {
+        if(Phaser.Geom.Intersects.RectangleToRectangle(dragon.getBounds(), birds[i].getBounds())) {
             // heartTween.play();
     
             smash.play();
-            deadFrog.setPosition(froggy.x, froggy.y);
+            deadFrog.setPosition(dragon.x, dragon.y);
             deadFrog.setVisible(true);
 
             resetFrogPosition();
             
         };
 
-        if(Phaser.Geom.Intersects.RectangleToRectangle(froggy.getBounds(), twoCar[i].getBounds())) {
+        if(Phaser.Geom.Intersects.RectangleToRectangle(dragon.getBounds(), otherBirds[i].getBounds())) {
             // heartTween.play();
     
             smash.play();
-            deadFrog.setPosition(froggy.x, froggy.y);
+            deadFrog.setPosition(dragon.x, dragon.y);
             deadFrog.setVisible(true);
 
             resetFrogPosition();
@@ -261,20 +269,20 @@ function update() {
 function resetFrogPosition(){
 
     deadFrog.setVisible(false);
-    froggy.setPosition(8+Phaser.Math.Between(0, 29)*16, 312);
-    froggy.angle= 0;
+    dragon.setPosition(12+Phaser.Math.Between(0, 55)*16, 620);
+    dragon.angle= 0;
     
 }
 
 function resetMoneyPosition(){
 
-    money[0].setPosition(Phaser.Math.Between(0, 450),235);
+    money[0].setPosition(Phaser.Math.Between(12, 950),60);
 
-    money[1].setPosition(Phaser.Math.Between(0, 450),175);
+    money[1].setPosition(Phaser.Math.Between(12, 950),195);
 
-    money[2].setPosition(Phaser.Math.Between(0, 450),110);
+    money[2].setPosition(Phaser.Math.Between(12, 950),320);
 
-    money[3].setPosition(Phaser.Math.Between(0, 450),30);
+    money[3].setPosition(Phaser.Math.Between(12, 950),445);
     
 }
 
@@ -292,9 +300,9 @@ function countingDown(){
         insertNewScoreInBestScores(score);
         resetMoneyPosition();
 
-        bestScoreText1.text= "Premier: "+ bestScores[0];
-        bestScoreText2.text= "Deuxieme: "+ bestScores[1];
-        bestScoreText3.text= "Troisième: "+ bestScores[2];
+        bestScoreText1.text= "First : "+ bestScores[0];
+        bestScoreText2.text= "Second : "+ bestScores[1];
+        bestScoreText3.text= "Third : "+ bestScores[2];
 
     
         countDownTimer.paused= false;
@@ -316,10 +324,10 @@ function startGame(){
     countDown= STARTCOUNTDOWN;
     countDownText.text= countDown + 's';
 
-    froggy.setPosition(8+Phaser.Math.Between(0, 29)*16, 312);
-    froggy.angle= 0;
+    dragon.setPosition(8+Phaser.Math.Between(0, 29)*16, 610);
+    dragon.angle= 0;
 
-    mumFrog.setPosition(Phaser.Math.Between(1, 25)*16, 8);
+    cave.setPosition(Phaser.Math.Between(1, 25)*16, 28);
 
     playing= false;
 
@@ -330,3 +338,4 @@ function insertNewScoreInBestScores(newScore){
     bestScores.push(newScore);
     bestScores = bestScores.sort(function(a,b){return a-b}).reverse().slice(0,3);
 }
+
